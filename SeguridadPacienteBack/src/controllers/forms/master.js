b15da6388838cb36e4881bb7ee16710340ce9bdc
Jose Ojeda <jlojeda0903@gmail.com>
2022-07-02 14:53:11 -0500

    feat: Agregar Empresa y Sede a getAnswers Master

--- SeguridadPacienteBack/src/controllers/forms/master.js
+++ SeguridadPacienteBack/src/controllers/forms/master.js
@@ -24,7 +24,7 @@ exports.createEntry = async (req, res) => {
 
 exports.getAnswers = async (req, res) => {
   const {
-    Id, Numero_Id, Start_Date, End_Date, Tipo_Novedad,
+    Id, Numero_Id, Start_Date, End_Date, Tipo_Novedad, Empresa, Sede,
   } = req.body;
   try {
     const answers = await MasterModel.findAll({
@@ -40,6 +40,8 @@ exports.getAnswers = async (req, res) => {
             },
           },
           { Tipo_Novedad },
+          { Empresa },
+          { Sede },
         ],
       },
       order: [["Fecha_Incidente", "ASC"]],
