6cb786f95f7874dd8e32bf121c3a54bc9e74add3
Jose Ojeda <jlojeda0903@gmail.com>
2022-07-03 16:04:12 -0500

    feat: Investigaciones Naranjo y Londres

--- SeguridadPacienteBack/src/routes/forms/investigaciones.js
+++ SeguridadPacienteBack/src/routes/forms/investigaciones.js
@@ -6,8 +6,17 @@ const router = express.Router();
 
 const {
   createInvM5,
+  createInvP5,
+  createInvNaranjo,
+  createInvLondres,
   getInvM5,
+  getInvP5,
+  getInvNaranjo,
+  getInvLondres,
   deleteInvM5,
+  deleteInvP5,
+  deleteInvNaranjo,
+  deleteInvLondres,
 } = require("../../controllers/forms/investigaciones");
 
 router.route("/5m")
@@ -18,5 +27,32 @@ router.route("/5m/registros")
 
 router.route("/5m/borrar")
   .post(deleteInvM5);
+
+router.route("/5p")
+  .post(createInvP5);
+
+router.route("/5p/registros")
+  .post(getInvP5);
+
+router.route("/5p/borrar")
+  .post(deleteInvP5);
+
+router.route("/naranjo")
+  .post(createInvNaranjo);
+
+router.route("/naranjo/registros")
+  .post(getInvNaranjo);
+
+router.route("/naranjo/borrar")
+  .post(deleteInvNaranjo);
+
+router.route("/londres")
+  .post(createInvLondres);
+
+router.route("/londres/registros")
+  .post(getInvLondres);
+
+router.route("/londres/borrar")
+  .post(deleteInvLondres);
 
 module.exports = router;
