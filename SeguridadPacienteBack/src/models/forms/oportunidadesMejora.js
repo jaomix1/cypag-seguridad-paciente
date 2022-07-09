b4020f5297900524306b8981918eb22cda0fafae
Jose Ojeda <jlojeda0903@gmail.com>
2022-07-09 14:55:41 -0500

    feat: Upd Model OpMejora y upd met Busqueda

--- SeguridadPacienteBack/src/models/forms/oportunidadesMejora.js
+++ SeguridadPacienteBack/src/models/forms/oportunidadesMejora.js
@@ -5,9 +5,17 @@ class OportunidadesMejoraModel extends Model {}
 
 OportunidadesMejoraModel.init(
   {
-    Id: { type: DataTypes.UUID, defaultValue: sequelize.literal("newid()"), primaryKey: true },
+    Id: {
+      type: DataTypes.UUID,
+      defaultValue: sequelize.literal("newid()"),
+      primaryKey: true,
+    },
     Id_Master: { type: DataTypes.UUID, allowNull: false },
-    Codigo_Externo: { type: DataTypes.STRING(30), allowNull: true, unique: true },
+    Codigo_Externo: {
+      type: DataTypes.STRING(30),
+      allowNull: true,
+      unique: true,
+    },
     Descripcion: { type: DataTypes.STRING(500), allowNull: true },
     Porcentaje_Mejora: {
       type: DataTypes.INTEGER,
@@ -17,10 +25,21 @@ OportunidadesMejoraModel.init(
     },
     Responsable: { type: DataTypes.STRING(100), allowNull: true },
     Estado: {
-      type: DataTypes.STRING(3), defaultValue: "ACT", allowNull: false, validate: { isIn: [["ACT", "INA"]] },
+      type: DataTypes.STRING(3),
+      defaultValue: "ACT",
+      allowNull: false,
+      validate: { isIn: [["ACT", "INA"]] },
     },
-    Fecha_Creacion: { type: DataTypes.DATE, defaultValue: sequelize.literal("getdate()"), allowNull: false },
-    Fecha_Modificacion: { type: DataTypes.DATE, defaultValue: null, allowNull: true },
+    Fecha_Creacion: {
+      type: DataTypes.STRING(50),
+      defaultValue: sequelize.literal("FORMAT (getdate(), 'yyyy-MM-dd HH:mm:ss.fff')"),
+      allowNull: false,
+    },
+    Fecha_Modificacion: {
+      type: DataTypes.DATE,
+      defaultValue: null,
+      allowNull: true,
+    },
   },
   {
     sequelize,
